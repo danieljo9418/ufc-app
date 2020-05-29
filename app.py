6c0ea9a0278ea354409d2b5d112a57399157b9c3
danieljo9418 <55205128+danieljo9418@users.noreply.github.com>
2020-05-29 18:43:39 -0500

    updated app

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def main():
         input_variables = pd.DataFrame([[BPrev, BStreak, B_Age,B_Height,B_Weight,RPrev, RStreak, R_Age,R_Height,R_Weight]],
                                        columns=['BPrev','BStreak','B_Age','B_Height','B_Weight','RPrev','RStreak','R_Age','R_Height','R_Weight'],
                                        dtype=float)
-                                    #    index=['input'])
+                                       index=['input'])
         prediction = model.predict(input_variables)[0]
         return flask.render_template('main.html',
                                      original_input={'R_Weight' :R_Weight,
